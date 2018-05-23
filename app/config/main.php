33557b5ec6d619b786053f0593f75baa9f022728
Sergey96 <sergey___96@mail.ru>
2018-05-23 20:21:42 +1000

    Файл конфигурации приложения

---
+++ app/config/main.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Главный файл конфигурации
+ */
+return [
+	'db'=>[
+		'host'=>'localhost',
+		'login'=>'root',
+		'password'=>'',
+		'dbname'=>'light',
+	],
+	'controller'=>[
+		'default'=>'Home'
+	],
+	'home'=>str_replace("\\", "/", $_SERVER['DOCUMENT_ROOT']),
+];
+
+?>
