5ed9619812503cc215d96855e3e2da1692ff6355
Sergey96 <sergey___96@mail.ru>
2018-06-07 20:57:45 +1000

    Создан Базовый Стилизатор)

---
+++ engine/base/Asset/Asset.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace engine\base\Asset;
+
+class Asset
+{
+	protected static $StyleTemplate = '
+		<link rel="stylesheet" href="{href}">';
+	
+	protected static $JSTemplate = '
+		<script src="{src}"></script>';
+
+	public static function addStyle($css){
+		$CSS = '';
+		foreach($css as $href){
+			$CSS .= str_replace('{href}', $href, Asset::$StyleTemplate);
+		}
+		return $CSS;
+	}
+
+	public static function addJS($js){
+		$JS = '';
+		foreach($js as $href){
+			$JS .= str_replace('{src}', $href, Asset::$JSTemplate);
+		}
+		return $JS;
+	}
+
+}
+
+?>
