29f840e8c9454dbd04f9fe59f49f1674c2f8c165
Sergey96 <sergey___96@mail.ru>
2018-06-07 20:38:09 +1000

    Снижено количество ошибок при загрузке данных Похоже не все еще обнаружены

--- engine/Models/Model.php
+++ engine/Models/Model.php
@@ -4,36 +4,70 @@ namespace engine\Models;
 
 use engine\WebApp;
 use engine\base\Exceptions as Exceptions;
-
+/**
+ *  Базовый Класс Модели
+*/
+/// Базовый Класс Модели
 class Model 
 {
+	/// Список полей модели
 	protected $attributes_;
+	/// Имя таблицы модели
 	protected $tableName;
+	private $ErrorLoad = false;
 	
+	/**
+	 * Установить имя таблицы
+	*/
 	public function setTableName($table){
 		$this->tableName = $table;
 	}
-
-	public function loadData($array){
+	
+	public function getErrorsLoad(){
+		return $this->ErrorLoad;
+	}
+	
+	/**
+	 * Загрузить данные в модель из массива
+	*/
+	public function load($array){
 		if(!is_array($array))
 			throw new \Exception("Не Удается загрузить данные в модель");
-		$properties = $this->getProperty();
-		$countLoadedElem = 0;
-		
-		foreach($properties as $k => $v){
-			if(isset($array[$v])){
-				$this->$v = $array[$v];
-				$countLoadedElem++;
+		$class = get_called_class();
+		$rules = $class::$attributeLabels;
+		foreach($rules as $k => $v){
+			if($rules[$k][2] == 'required' && !isset($array[$k])){
+				$this->ErrorLoad = 'Отсутствует обязательное поле: '.$k;
+				return false;
+			}
+			if(isset($array[$k]) && strlen($array[$k])!=0){
+				if(($rules[$k][1] == 'text' || $rules[$k][1] == 'datetime') && !is_string($array[$k])){
+					$this->ErrorLoad = 'Поле не соответствует формату: '.$k.' => '.$rules[$k][1];
+					return false;
+				}
+				if($rules[$k][1] == 'int' && !is_numeric($array[$k])){
+					$this->ErrorLoad = 'Поле не соответствует формату: '.$k.' => '.$rules[$k][1];
+					return false;
+				}
+				$this->$k = $array[$k];
 			}
 		}
-		
-		if($countLoadedElem != count($array)-1){
-			return false;
-		}
+		if(isset($this->id))
+			$this->setNotNew();
 		return true;
 	}
 	
-	public function load($array){
+	public function getFieldList($rules){
+		$fields = array();
+		foreach($rules as $k => $v)
+			$fields[] = $k;
+		return $fields;
+	}
+	
+	/**
+	 * Загрузить данные в модель используя список полей из БД
+	*/
+	public function loadData($array){
 		if(!is_array($array))
 			throw new \Exception("Не Удается загрузить данные в модель");
 		$this->getTableColumns();
@@ -56,11 +90,6 @@ class Model
 		exit();
 		return true;
 	}
-	
-	public function getData($fieldList){
-		return $this->getDataFromDB($fieldList);
-	}
-	
 	
 	/**
      * Получение списка полей класса
@@ -101,6 +130,9 @@ class Model
 		return get_class($this);
 	}
 	
+	/**
+	 * Получить список полей в связанной таблице
+	 */
 	public function getTableColumns()
 	{
 		try 
@@ -110,14 +142,14 @@ class Model
 				print_r(WebApp::$connection->getErrors());
 				throw new Exceptions\DataBaseException(WebApp::$connection->getErrors()[2].' '.$this->tableName);
 			}
-			$columns = $query->fetchAll(\PDO::FETCH_CLASS, "engine\Components\Gii\models\Column");
+			$columns = $query->fetchAll(\PDO::FETCH_CLASS, "engine\components\Gii\models\Column");
 			foreach($columns as $column){
 				$this->attributes_[$column->Field] = $column;
 				$this->attributes_[$column->Field]->Value = 0;
 			}
 		}
 		catch (\Exception $e){
-			print_r($e);
+			//print_r($e);
 			throw $e;
 		}
 	}
