4c55518a893969115d13ae98301e60775a52677d
Sergey96 <sergey___96@mail.ru>
2018-06-07 20:47:59 +1000

    Убрал лишнее Поменьше ошибок

--- engine/Controller/Controller.php
+++ engine/Controller/Controller.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace engine\Controller;
-use app\assets\AppAsset;
 use engine\WebApp;
 use engine\base\Exceptions as Exceptions;
+use engine\views\View;
 /**
  * Базовый класс - Controller
  */
@@ -13,7 +13,6 @@ class Controller
 {
 	/// Имя контроллера
 	public $Name;
-	
 	/// Действие
 	public $Action;
 	/// Текущий URL
@@ -22,15 +21,15 @@ class Controller
 	public $ViewPath  = '../views/';
 	/// Имя шаблона views
 	public $Layout;
-	/// Поле не используется 
-	public $config;
 	/// Массив параметров (breadcrumbs и т.д.)
 	public $params;	
 	/// Заголовок html-страницы
 	public $title;
-	/// GET и POST - массивы
-	public $request;
 
+	public function accessRights(){
+		return array();
+	}
+	
     /**
      * Конструктор класса
      *
@@ -50,6 +49,15 @@ class Controller
      */
 	public function action(){
 		$this->render('index');
+	} 
+
+	/**
+     * Действие для controller/index (рендерниг index-страницы)
+     *
+     * @return string
+     */
+	public function actionIndex(){
+		$this->render('index');
 	}
 	
     /**
@@ -58,50 +66,50 @@ class Controller
      * @return string
      */
 	public function error($exception){
-		$this->title = $exception->getCode().": ".$exception->getMessage();
+		
 		$this->render('error', [
+			'title' => $exception->getTitle(),
 			'message'=>$exception->getMessage(),
 			'code'=>$exception->getCode(),
-			'objError'=>$exception->getElement()
+			'objError'=>$exception->getElement(), 
+			'file'=>$exception->getFile(),
+			'line'=>$exception->getLine(),
+			'exception'=>$exception
 		]);
 	}
-
+	
+	
     /**
-     * Действие для controller/index (рендерниг index-страницы)
+     * Функция выбора действия
      *
      * @return string
      */
-	public function actionIndex(){
-		$this->render('index');
-	}
-
-    /**
-     * Возвращает содержимое view - файла
-     *
-     * @return string
-     */
-	protected function get_include_contents($filename, $param) {
-		foreach($param as $k => $v){
-			${$k} = $v;
+	public function selectAction(){
+		$action = $this->getActionMethod();
+		$Metods = $this->getMethods();
+		$key = array_search($action, $Metods);
+		if($key === false){
+			throw new Exceptions\ActionNotFoundException($this->Action);
+		} 
+		else {
+			$right = WebApp::$controller->accessRights();
+			if(!WebApp::$user->can() && WebApp::$user->isRule('*')){
+				throw new Exceptions\ForbiddenException($this->Action);
+			}
+			if(!WebApp::$user->can())
+			
+				$this->redirect([$right['redirect']['view']], $right['redirect']['controller']);
+			
+			$params = $this->getArgsForMetod($action);	
+			$count = count($params);
+		
+			if($count>0){
+				call_user_func_array(array($this, $action), $params);
+			}
+			else {
+				$this->$action();
+			}
 		}
-		if (is_file($filename)) {
-			ob_start();
-			include $filename;
-			$contents = ob_get_contents();
-			ob_end_clean();
-			return $contents;
-		}
-		return false;
-	}
-
-    /**
-     * Возвращает содержимое view - файла
-     *
-     * @return string
-     */
-	protected function getContentView($path, $param){
-		$string = $this->get_include_contents($path, $param);
-		return  $string;
 	}
 
     /**
@@ -109,25 +117,39 @@ class Controller
      *
      * @return string
      */
-	public function render($view, $param = array()){
+	protected function render($view, $param = array()){
+		$viewObj = new View($view, $this->ViewPath, $this->Name, $this->URL);
 		$layout = $this->ViewPath.'/layout/'.$this->Layout;
-		$view = $this->ViewPath . $this->Name."/$view.php";
+		ob_start();
 		try{
-			$content = $this->getContentView($view, $param);
+			$content = $viewObj->render($view, $param);
+			$this->title = $viewObj->getTitle();
+			$this->params = $viewObj->getParams();
 			require_once $layout;
 		}
-		catch (\Exeption $e){
-			echo 'Не найден файл';
+		catch(\Exception $e){
+			$content = '';
+			ob_end_clean();
+			throw $e;
 		}
+		$contents = ob_get_clean();
+		print_r($contents);
 	}
 	
-	public function redirect($param){
-		header('HTTP/1.1 200 OK');
-		header('Location: '.$this->arrayToURL($param));
+	protected function redirect($param, $controller = false){
+		if($controller===false)
+			$controller = $this->Name;
+		else
+			$controller = $this->Name;
+		$code = '200 OK';
+		if(isset($param['code']))
+			$code = $param['code'];
+		header('HTTP/1.1 '.$code);
+		header('Location: '.$this->arrayToURL($param, $controller));
 	}
 	
-	private function arrayToURL($param){
-		$url = 'http://'.$_SERVER['HTTP_HOST'].'/'.$this->Name.'/'.$param[0];
+	private function arrayToURL($param, $controller){
+		$url = 'http://'.$_SERVER['HTTP_HOST'].'/'.$controller.'/'.$param[0];
 		$count = 0;
 		print_r($param);
 		if(count($param)>1)
@@ -137,38 +159,21 @@ class Controller
 			if($count==1) continue;
 			$url .= $k.'='.$p.'&';
 		}
-		return str_replace("\\", "/", substr($url, 0, strlen($url)-1));
+		if(count($param)>1)
+			$url = substr($url, 0, strlen($url)-1);
+		return str_replace("\\", "/", $url);
 	}
 
-    /**
-     * Функция выбора действия
-     *
-     * @return string
-     */
-	public function SelectAction(){
-		
-		$action = 'action'.mb_strtoupper(mb_substr($this->Action, 0, 1), 'UTF-8').mb_substr($this->Action, 1, strlen($this->Action)-1, 'UTF-8');
-		$Metods = $this->getMethods();
-		$key = array_search($action, $Metods);
-		//print_r($this->Action);
-		if($key === false){
-			throw new Exceptions\ActionNotFoundException($this->Action);
-		} 
-		else {
-			$params = $this->getArgsForMetod($action);	
-			$count = count($params);
-			if($count>0){
-				call_user_func_array(array($this, $action), $params);
-			}
-			else {
-				$this->$action();
-			}
-		}
+	private function getActionMethod(){
+		$action = 'action'.mb_strtoupper(mb_substr($this->Action, 0, 1), 'UTF-8');
+		$action .= mb_substr($this->Action, 1, strlen($this->Action)-1, 'UTF-8');
+		return $action;
 	}
 	
+	
 	private function getArgsForMetod($action){
-		$class  = new \ReflectionClass( $this );
-		$method = $class->getMethod( $action );
+		$class  = new \ReflectionClass($this);
+		$method = $class->getMethod($action);
 		$param  = $method->getParameters();
 		$count = count($param);
 		$args = array();
@@ -176,7 +181,6 @@ class Controller
 		$notFound = null;
 		for($i=0; $i<$count; $i++)
 		{
-			$flag = false;
 			$notFound = $param[$i]->name;
 			foreach($GET as $name => $value)
 			{
@@ -193,12 +197,6 @@ class Controller
 		
 		return $args;
 	}
-	
-	private function isIssetAllArgs($params){
-		print_r($params);
-		print_r(WebApp::$request->get());
-		return false;
-	}
 
     /**
      * Возвращает список методов класса
@@ -209,15 +207,22 @@ class Controller
 		return get_class_methods($this);
 	}
 
-    /**
-     * Функция вывода конечной части body (js, css и т.д.)
-     *
-     * @return string
-     */
-	public function endBody()
+	
+	public static function startHead()
     {
-        $appAssest = new AppAsset();
-        print_r($this->params);
+		View::startHead();
+    }
+	
+	public static function endHead()
+    {
+		$Asset = '\\'.WebApp::$config['namespace'].'\\assets\\AppAsset';
+		View::endHead($Asset);
+    }
+	
+	public static function endBody()
+    {
+		$Asset = '\\'.WebApp::$config['namespace'].'\\assets\\AppAsset';
+		View::endBody($Asset);
     }
 }
 
