096b9c401fd863ddcef3b1488c12a92f0739d211
Sergey96 <sergey___96@mail.ru>
2018-06-07 20:45:35 +1000

    Виджет стал рабочим))

--- engine/widgets/Breadcrumbs.php
+++ engine/widgets/Breadcrumbs.php
@@ -2,29 +2,60 @@
 
 namespace engine\widgets;
 
+/**
+ * Виджет - хлебные крошки
+ * отображение иерархии разделов сайта
+ */
+/// Виджет - хлебные крошки
 class BreadCrumbs
 {
-	public $template = "<li><a href='{url}'>{label}</a></li> > ";
-	public $templateNotLink = "<li>{label}</li>";
+	/**
+	 * Шаблон для элемента - ссылки
+	 */
+	public $template = "
+	<li><a href='{url}'>{label}</a></li> ";
+	
+	/**
+	 * Шаблон для последнего элемента, обычный текст
+	 */
+	public $templateNotLink = "
+	<li>{label}</li>";
+	
+	/**
+	 * Метка, текст ссылки
+	 */
 	public $label;
+	
+	/**
+	 * URL - адрес
+	 */
 	public $url;
 
-	public static function printBreadCrumbs($params){
+	/**
+	 * Вывести хлебные крошки
+	 */
+	public static function View($params){
 		$bread = new BreadCrumbs();
 		$html = '<ul>';
 		
 		foreach($params as $k => $v){
 			if(is_array($v)){
 				$u = str_replace("{url}", $v['url'], $bread->template);
-				$l = str_replace("{label}", $v['label'], $u);
+				
+				$l = str_replace("{label}", self::getLabel($v['label']), $u);
 			}
 			else {
-				$l = str_replace("{label}", $v, $bread->templateNotLink);
+				$l = str_replace("{label}",  self::getLabel($v), $bread->templateNotLink);
 			}
 			$html .= $l;
 		}
 		$html .= '</ul>';
 		print_r($html);
+	//	exit();
+	}
+	
+	private static function getLabel($label){
+		return $label;
 	}
 
 }
