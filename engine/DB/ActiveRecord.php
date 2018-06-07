7d4b7269ba2b26cd45911824ee83508bf790a51e
Sergey96 <sergey___96@mail.ru>
2018-06-07 20:34:51 +1000

    Дополненная ActiveRecord в следующем коммите удалю около 10 методов

--- engine/DB/ActiveRecord.php
+++ engine/DB/ActiveRecord.php
@@ -2,10 +2,12 @@
 
 namespace engine\DB;
 
-use engine\DB\DataBase;
+use engine\WebApp;
+use engine\models\Model;
+use engine\base\Exceptions as Exceptions;
 
 /// Класс записи в БД
-class ActiveRecord extends DataBase
+class ActiveRecord extends Model implements ActiveRecordInterface
 {
     /// Связанная таблица в БД
 	public $Table;
@@ -13,6 +15,8 @@ class ActiveRecord extends DataBase
 	protected $isNew;
 	/// Объект запроса к БД
 	private $queryObj;
+	/// Поля с данными, не соответствующих типов
+	private $fieldsErrorType;
     /**
      * Конструктор класса
      *
@@ -20,6 +24,14 @@ class ActiveRecord extends DataBase
      */
 	public function __construct(){
 		$this->isNew = 1;
+	}
+	
+	public function __set($name, $value) {
+        $this->$name = $value;
+    }
+	
+	public function setNotNew(){
+		return $this->isNew = 0;
 	}
 
     /**
@@ -46,9 +58,63 @@ class ActiveRecord extends DataBase
      * @return string
      * @throws \ReflectionException
      */
-	public function save(){
-		$class = $this->generateQuery();
-		return $class;
+	public function save($validate = true){
+		if($this->validate($validate)){
+			$Query = $this->generateQuery();
+			
+			$stmt = $this->prepare($Query);
+			$stmt->execute($this->getDataAsArray($this->isNew));
+			
+			
+			$errors = $stmt->errorInfo();
+			if($errors[1])
+				throw new Exceptions\DatabaseException($errors[2]);
+		}
+		else throw new Exceptions\ErrorFieldTypeException($this->fieldsErrorType);
+	}
+	
+	public function delete(){
+		$Query = $this->deleteSQL();
+		$this->exeQuery($Query);
+		$errors = $this->getErrorsDB();
+		if($errors[1])
+			throw new Exceptions\DatabaseException($errors[2]);
+	}
+	
+	public function validate($validate){
+		
+		if($validate){
+			$issetErrors = false;
+			foreach($this::$attributeLabels as $field => $properties){
+				if(strlen($this->$field)==0 && $properties[2]=='required'){
+					throw new Exceptions\EmptyRequiredFieldException($field);
+				} 
+				else continue;
+				
+				if($properties[1]=='int' && is_numeric($this->$field)){
+					continue;
+				} 
+				else throw new Exceptions\InvalidDataException($field);
+				
+				if($properties[1]=='text' && is_string($this->$field)){
+					continue;
+				} 
+				else throw new Exceptions\InvalidDataException($field);
+				
+				if($properties[1]=='datetime' && validateDate($this->$field)){
+					continue;
+				} 
+				else throw new Exceptions\InvalidDataException($field);
+			}
+			return true;
+		}
+		else return true;
+	}
+	
+	public function validateDate($date, $format = 'Y-m-d H:i:s')
+	{
+		$d = date_parse_from_format($format, $date);
+		return $d['warning_count'] || $d['error_count'];
 	}
 
     /**
@@ -57,13 +123,13 @@ class ActiveRecord extends DataBase
      * @return string
      * @throws \ReflectionException
      */
-	protected function generateQuery(){
-		$p = $this->getProperty();
+	public function generateQuery(){
 		if($this->isNew){
-			return $this->insert($p);
+			return $this->insertSQL();
 		}
-		else
-		    return $this->update($p);
+		else {	
+		    return $this->updateSQL();
+		}
 	}
 
     /**
@@ -71,12 +137,67 @@ class ActiveRecord extends DataBase
      *
      * @return string
      */
-    protected function insert($property){
-        $StringProperty = $this->getStringFromArray($property);
-        $StringData = $this->getStringFromObj($this);
-        $query = "INSERT INTO `". $this->Table ."` ($StringProperty) VALUES ($StringData)";
+    public function insertSQL(){
+        $fields = $this->getFieldsAsString();
+        $data = $this->getFieldsForValueAsString();
+        $query = "INSERT INTO `". $this->Table ."` ($fields) VALUES ($data)";
         return $query;
     }
+	
+	public function deleteSQL(){
+        $query = "DELETE FROM `". $this->Table ."` WHERE id = ".$this->id;
+        return $query;
+    }
+	
+	public function getFieldsAsString(){
+		$string ='';
+		foreach($this::$attributeLabels as $k => $v)
+		{
+			if($v[2]=='autoincrement')
+				continue;
+			if(strlen($string)==0)
+				$string .= "`".$k."`";
+			else
+				$string .= ", `".$k."`";
+		}
+		return $string;
+	}
+	
+	public function getFieldsForValueAsString(){
+		$string ='';
+		foreach($this::$attributeLabels as $k => $v)
+		{
+			if($v[2]=='autoincrement')
+				continue;
+			if(strlen($string)==0)
+				$string .= ":$k";
+			else
+				$string .= ", :$k";
+		}
+		return $string;
+	}
+	
+	public function getDataAsArray($isNew){
+		$data = array();
+		foreach($this::$attributeLabels as $k => $v)
+		{
+			if($v[2]=='autoincrement' && $isNew){
+				continue;
+			}
+			$data[$k] = $this->$k;
+		}
+		return $data;
+	}
+	
+	
+	public function getFields(){
+		$data = array();
+		foreach($this::$attributeLabels as $k => $v)
+		{
+			$data[] = $k;
+		}
+		return $data;
+	}
 
     /**
      * Генерирование UPDATE - SQL запроса
@@ -84,12 +205,25 @@ class ActiveRecord extends DataBase
      * @todo Исправить функцию
      * @return string
      */
-    protected function update($property){
-        $StringProperty = $this->getStringFromArray($property);
-        $StringData = $this->getStringFromObj($this);
-        $query = "UPDATE `". $this->Table ."` SET  ($StringProperty) VALUES ($StringData)";
+    public function updateSQL(){
+		$fields = $this->getFieldsForUpdateAsString();
+        $query = "UPDATE `". $this->Table ."` SET  $fields WHERE id = :id";
         return $query;
     }
+	
+	public function getFieldsForUpdateAsString(){
+		$string ='';
+		foreach($this::$attributeLabels as $k => $v)
+		{
+			if($k=='id')
+				continue;
+			if(strlen($string)==0)
+				$string .= "$k = :$k";
+			else
+				$string .= ", $k = :$k";
+		}
+		return $string;
+	}
 
     /**
      * Генерирование SELECT - SQL запроса
@@ -98,10 +232,117 @@ class ActiveRecord extends DataBase
      */
 	public function select($field){
 		$query = "SELECT ";
-		$query .= $this->getStringFromArray($field);
-		$query .= " FROM ".substr($this->Table, 0, -5);
+		if(is_array($field))
+			$query .= $this->getStringFromArray($field);
+		else
+			$query .= $field;
+		$query .= " FROM `".$this->Table."`";
 
 		return $query;
+	}
+	
+	/**
+     * Получить данные в виде массива моделей
+     *
+     * @return string
+     */
+	public function getData($fieldList,  $limit){
+		$query = $this->select($fieldList); 
+		$query = $this->limit($query, $limit);
+		print_r($query);
+		//exit();
+		$result = $this->exeQuery($query);
+		if (!$result) {
+			throw new Exceptions\DataBaseException(WebApp::$connection->getErrors()[2].' '.$this->Table);
+		}
+		return $result->fetchAll(\PDO::FETCH_CLASS, $this->getClassName());
+	}
+	
+	public function limit($query, $limit){
+		return $query . " LIMIT ".$limit;
+	}
+	
+	public function where($query, $array){
+		$where = ' WHERE ';
+		foreach($array as $operator => $operands){
+			switch($operator){
+				case "=": $where .= " `".$operands[0]."` $operator '".$operands[1]."' ";
+			}
+		}
+		return $query . $where;
+	}
+	/*
+	['=' => ['id', '1']
+	['and' => ['id', '1']]
+	['or' => [
+		['=' => ['id', '1']],
+		['=' => ['id', '2']]
+	]]
+	['id', '1']]
+	*/
+	public function getByID($id)
+	{
+		return $this->getByField('id', $id);
+	}
+	
+	public function getByField($field, $value)
+	{
+		$query = $this->select("*"); 
+		$query = $this->where($query, ['=' => [$field, $value]]);
+		
+		$result = $this->exeQuery($query);
+		if (!$result) {
+			print_r(WebApp::$connection->getErrors());
+			throw new Exceptions\DataBaseException(WebApp::$connection->getErrors()[2].' '.$this->Table);
+		}
+		$model = $result->fetchAll(\PDO::FETCH_CLASS, $this->getClassName());
+		
+		if(count($model)==0){
+			$class = $this->getClassName();
+			return new $class();
+		}
+		else {
+			$model[0]->isNew = false;
+			return $model[0];	
+		}
+	}
+	
+	public function getAllRecordsByField($field, $value)
+	{
+		$query = $this->select("*"); 
+		$query = $this->where($query, ['=' => [$field, $value]]);
+		
+		$result = $this->exeQuery($query);
+		if (!$result) {
+			print_r(WebApp::$connection->getErrors());
+			throw new Exceptions\DataBaseException(WebApp::$connection->getErrors()[2].' '.$this->Table);
+		}
+		$model = $result->fetchAll(\PDO::FETCH_CLASS, $this->getClassName());
+		
+		if(count($model)==0){
+			$class = $this->getClassName();
+			return [new $class()];
+		}
+		else {
+			foreach($model as $v)
+				$v->isNew = false;
+			return $model;	
+		}
+	}
+	
+	
+	public static function findByField($field, $value)
+	{
+		$class = get_called_class();
+		$model = new $class();
+		return $model->getByField($field, $value);
+	}
+	
+	public static function findByID($id)
+	{
+		$class = get_called_class();
+		$model = new $class();
+		return $model->getByID($id);
 	}
 
     /**
@@ -109,7 +350,7 @@ class ActiveRecord extends DataBase
      *
      * @return string
      */
-    protected function getStringFromObj($obj){
+    public function getStringFromObj($obj){
 		$p = $obj->getProperty();
 		$str = '';
 		foreach($p as $k => $v){
@@ -126,15 +367,19 @@ class ActiveRecord extends DataBase
      *
      * @return string
      */
-	protected function getStringFromArray($array){
+	public function getStringFromArray($array){
 		$str ='';
+		
 		foreach($array as $k =>$v)
 		{
 			if($k==0)
-				$str .= "'".$v."'";
+				$str .= "`".$v."`";
 			else
-				$str .= ", '".$v."'";
+				$str .= ", `".$v."`";
 		}
+		
+		//print_r("FIELDLIST");
+		//print_r($str);
 		return $str;
 	}
 
@@ -143,16 +388,25 @@ class ActiveRecord extends DataBase
      *
      * @return string
      */
-	protected function getDataFromDB($field){
+	public function getDataFromDB($field){
 		$fields = array();
 		foreach($field as $v)
 			$fields[] = $v[0];
 		return $this->select($fields);
 	}
 	
-	protected function exeQuery($query){
-		WebApp::connection->executeQuery();
+	public function exeQuery($query){
+		return WebApp::$connection->executeQuery($query);
 	}
+	
+	public function prepare($query){
+		return WebApp::$connection->prepare($query);
+	}
+	
+	public function getErrorsDB(){
+		return WebApp::$connection->getErrors();
+	}
+
 }
 
 ?>
