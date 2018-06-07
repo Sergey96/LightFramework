356b11aebd19e5558b078242f26b0b66b0ed318c
Sergey96 <sergey___96@mail.ru>
2018-06-07 20:36:06 +1000

    Соединение создается только когда потребуется Добавлены prepare-запросы

--- engine/DB/DataBase.php
+++ engine/DB/DataBase.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace engine\DB;
-use engine\Models\Model;
 
+use engine\Models\Model;
+use engine\DB\DBManager;
+
+/**
+ * DataBase - Класс для работы с БД
+ */
 class DataBase extends Model
 {
 	private $Connection;
@@ -10,22 +15,57 @@ class DataBase extends Model
 	private $Login;
 	private $Password;
 	private $DBName;
+	private $Status;
+	private $Config;
+	
     /**
-     * Login action.
-     *
-     * @return string
+     * Конструктор класса, создает соединение с БД
      */
 	public function __construct($config){
-		$this->Connection = new \PDO("mysql:host=".$config['host'].";dbname=".$config['dbname'], $config['login'], $config['password']);
+		$this->Config = $config;
+		$this->Status = false;
 	}
 	
+	/**
+	* Инициализация
+	*/
+	
+	private function Init(){
+		if($this->Status==false){
+			$this->Connection = new \PDO
+			(
+				"mysql:host=".$this->Config['host'].";dbname=".$this->Config['dbname'], 
+				$this->Config['login'], $this->Config['password'], 
+				array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
+			);
+			$this->Status = true;
+		}
+	}
+	
+    /**
+     * Выполнение произвольного SQL-запроса
+     */
 	public function executeQuery($query){
+		$this->Init();
 		return $this->Connection->query($query);
 	}
 	
+	/**
+     * Подготовка \PDO->SQL - запроса
+     */
+	public function prepare($query){
+		$this->Init();
+		return $this->Connection->prepare($query);
+	}
+	
+    /**
+     * Просмотр ошибок при выполнении запросов к БД
+     */
 	public function getErrors(){
+		$this->Init();
 		return $this->Connection->errorInfo();
 	}
+
 }
 
 ?>
