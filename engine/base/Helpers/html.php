7beff235a31a3bd7147de155de19ac32556d81d0
Sergey96 <sergey___96@mail.ru>
2018-06-24 09:31:51 +1000

    Класс, для создания html тегов - модет перегонять строку в utf-8

---
+++ engine/base/Helpers/html.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace engine\base\Helpers;
+
+use engine\WebApp;
+
+class html
+{
+	public static function p($text){
+		
+	}
+
+	public static function toUTF8($text){
+		if(mb_check_encoding($text, 'utf8'))
+			return $text;
+		if(mb_check_encoding($text, 'windows-1251'))
+			return iconv('windows-1251', 'utf-8', $text);
+	}
+	
+}
