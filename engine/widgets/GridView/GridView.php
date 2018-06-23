f70a7903e3ed36e0aaf4ca41d93b186437c037f3
Sergey96 <sergey___96@mail.ru>
2018-06-24 09:51:42 +1000

    Добавлены три столбца для ссылок на действия со строками Надо переделать как ActiveForm на MVC замена значений в строках смотрится уродливо

--- engine/widgets/GridView/GridView.php
+++ engine/widgets/GridView/GridView.php
@@ -9,11 +9,24 @@ class GridView
 {
 	public $model;
 	public $fieldList;
-	public $templateTable = "<table {id} {class}>\n{header}<th>UPDATE</th><th><i class='icon-trash' title='Edit'> </i></th>\n{rows}\t</table>\n";
+	public $actions;
+	public $templateTable = "
+		<table {id} {class}>\n{header}
+			\n{rows}
+		\t</table>\n";
+	public $templateActionsHeader = [
+		'view'=>"<th class='table-icon'>&#128065;</th>",
+		'update'=>"<th class='table-icon'>&#x270e;</th>",
+		'delete'=>"<th class='table-icon'>&#x2718;</th>"
+		];
 	public $templateHeader = "<th>{headers}</th>";
 	public $templateRow = "\t\t<tr {id} {class}>\n{collumns}{actions}\n\t\t</tr>\n";
 	public $templateCollumns = "\t\t\t<td {id} {class}>{value}</td>\n";
-	public $templateActions = "<td><a href='/{controller}/update?id={id}'>update</a></td><td><a href='delete?id={id}'>delete</a></td>";
+	public $templateActions = [
+		'view'=>"<td><a href='/{controller}/view?id={id}'>view</a></td>",
+		'update'=>"<td><a href='/{controller}/update?id={id}'>update</a></td>",
+		'delete'=>"<td><a href='/{controller}/delete?id={id}'>del</a></td>"
+	];
 	public $RowsInPage = 25;
 	
 	public function __construct($param){
@@ -23,7 +36,6 @@ class GridView
 	
 	public function setSearchModel($params){
 		if(is_array($params) && isset($params['searchModel'])){
-			echo "Нашел модель";
 			$this->searchModel = $params['searchModel'];
 		}
 		else throw new Exceptions\ParameterNotFoundException("searchModel");
@@ -31,7 +43,6 @@ class GridView
 	
 	public function setDataProvider($params){
 		if(is_array($params) && isset($params['dataProvider'])){
-			echo "Нашел модель";
 			$this->dataProvider = $params['dataProvider'];
 		}
 		else throw new Exceptions\ParameterNotFoundException("dataProvider");
@@ -41,6 +52,9 @@ class GridView
 		$this->setSearchModel($p);
 		$this->setDataProvider($p);
 		$this->fieldList = $this->parseField($p['fields']);
+		if(isset($p['actions']))
+			$this->actions = $p['actions'];
+		else $this->actions = ['view', 'update', 'delete'];
 	}
 	
 	private function parseField($list){
@@ -58,41 +72,31 @@ class GridView
 		
 		$HEADER = false;
 		$headers = '';
-		foreach($this->fieldList as $field)
-			$headers .= str_replace("{headers}", $field, $this->templateHeader);
+		foreach($this->fieldList as $field){
+			$headers .= str_replace("{headers}", $this->searchModel::$attributeLabels[$field][0], $this->templateHeader);
+		}
+		$actions = '';
+		foreach($this->actions as $act)
+			$actions .= $this->templateActionsHeader[$act];
+		$headers .= $actions;
 		
 		$tr = "";
 		foreach($rows as $row_key => $row){
 			$td = "";
+			$actions = '';
 			foreach($this->fieldList as $field){
 				$td .= str_replace("{value}", $row->$field, $this->templateCollumns);
 			}
 			$tr .= str_replace("{collumns}", $td, $this->templateRow);
-			$actions = str_replace("{id}", $row->id, $this->templateActions);
+			foreach($this->actions as $act)
+				$actions .= str_replace("{id}", $row->id, $this->templateActions[$act]);
 			$actions = str_replace("{controller}", WebApp::$controller->Name, $actions);
 			$tr = str_replace("{actions}", $actions, $tr);
 		}
 		$table =  str_replace("{rows}", $tr, $this->templateTable);
 		$table =  str_replace("{header}", $headers, $table);
-		print_r($table);
-	}
-	
-	public static function printBreadCrumbs($params){
-		$bread = new BreadCrumbs();
-		$html = '<ul>';
 		
-		foreach($params as $k => $v){
-			if(is_array($v)){
-				$u = str_replace("{url}", $v['url'], $bread->template);
-				$l = str_replace("{label}", $v['label'], $u);
-			}
-			else {
-				$l = str_replace("{label}", $v, $bread->templateNotLink);
-			}
-			$html .= $l;
-		}
-		$html .= '</ul>';
-		print_r($html);
+		print_r($table);
 	}
 
 }
