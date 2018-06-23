51fc11f018611ea890760250ef078c35949be98b
Sergey96 <sergey___96@mail.ru>
2018-06-24 09:43:10 +1000

    Отказался от идеи инициализации по первому запросу

--- engine/DB/DataBase.php
+++ engine/DB/DataBase.php
@@ -24,6 +24,7 @@ class DataBase extends Model
 	public function __construct($config){
 		$this->Config = $config;
 		$this->Status = false;
+		$this->Init();
 	}
 	
 	/**
@@ -32,13 +33,23 @@ class DataBase extends Model
 	
 	private function Init(){
 		if($this->Status==false){
-			$this->Connection = new \PDO
-			(
-				"mysql:host=".$this->Config['host'].";dbname=".$this->Config['dbname'], 
-				$this->Config['login'], $this->Config['password'], 
-				array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
-			);
-			$this->Status = true;
+			try{
+				$opt = [
+					\PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+					\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+					\PDO::ATTR_EMULATE_PREPARES   => false,
+					\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+				];
+				$this->Connection = new \PDO
+				(
+					"mysql:host=".$this->Config['host'].";dbname=".$this->Config['dbname'],//.";'collation' => 'utf8'", 
+					$this->Config['login'], $this->Config['password'], $opt
+				);
+				$this->Status = true;
+			}
+			catch(\PDOException $e){
+				throw $e;
+			}
 		}
 	}
 	
@@ -46,7 +57,6 @@ class DataBase extends Model
      * Выполнение произвольного SQL-запроса
      */
 	public function executeQuery($query){
-		$this->Init();
 		return $this->Connection->query($query);
 	}
 	
@@ -54,7 +64,6 @@ class DataBase extends Model
      * Подготовка \PDO->SQL - запроса
      */
 	public function prepare($query){
-		$this->Init();
 		return $this->Connection->prepare($query);
 	}
 	
@@ -62,7 +71,6 @@ class DataBase extends Model
      * Просмотр ошибок при выполнении запросов к БД
      */
 	public function getErrors(){
-		$this->Init();
 		return $this->Connection->errorInfo();
 	}
 
